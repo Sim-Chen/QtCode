83c6b5e0580a5ae8fb1c71d5a326f70fb30286d5
EverNote <chenzhendoit@163.com>
2017-08-30 09:41:02 +0800

    1.添加Diagram程序

---
+++ Diagram_Pro/link.h
@@ -0,0 +1,26 @@
+﻿#ifndef LINK_H
+#define LINK_H
+#include<QGraphicsItem>
+
+class Node;
+
+class Link : public QGraphicsLineItem
+{
+public:
+    Link(Node *FromNode,Node *ToNode);
+    ~Link();
+    Node *fromNode()const;
+    Node *toNode()const;
+
+    void setColor(const QColor &color);
+    QColor color()const;
+
+
+   void trackNodes();
+
+private:
+    Node *myFromNode;
+    Node *myToNode;
+};
+
+#endif // LINK_H
